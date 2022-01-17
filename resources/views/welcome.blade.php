8414c050af458db4fe491bf391914be164a4e35c
shunsuke.konno <ubuntu@ip-172-31-35-248.ap-northeast-1.compute.internal>
2022-01-17 17:48:15 +0000

    post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,24 @@
 
 @section('content')
     @if(Auth::check())
-        {{ Auth::user()->name }}
-        @else
+        <div class="row">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-sm-8">
+                {{-- 投稿フォーム --}}
+                @include('microposts.microposts')
+            </div>
+        </div>
+    @else
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Welcome to the Microposts</h1>
